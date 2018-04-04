39f56059d78b8e27e161c257435e0b4181cdd6a7
Hemrana <35942809+Hemrana@users.noreply.github.com>
2018-04-04 13:52:33 -0400

    Update userprofile.sql

--- IT490 - Midterms/FrontEnd/profile/_sqlfile/userprofile.sql
+++ IT490 - Midterms/FrontEnd/profile/_sqlfile/userprofile.sql
@@ -1,28 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10.7
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jun 27, 2015 at 03:27 PM
--- Server version: 5.5.44-MariaDB
--- PHP Version: 5.4.31
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `infismas_aboutme`
---
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `user`
 --
 
