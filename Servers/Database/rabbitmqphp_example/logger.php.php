15d085a311734e13f54e93893f8e4ac51db20f04
Carlos Antonius <cm237@njit.edu>
2018-05-07 18:42:25 -0400

    Delete logger.php.php

--- Servers/Database/rabbitmqphp_example/logger.php.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = 'log_test.txt';
-// Open the file to get existing content
-$current = file_get_contents($file);
-// Append a new person to the file
-$current .= "This section is to be handled for error handling - append - on a RMQ Listener\n";
-// Write the contents back to the file
-file_put_contents($file, $current);
-?>
