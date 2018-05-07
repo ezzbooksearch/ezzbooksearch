32de90672cae9931cbe78d5487f6f02d75f54c67
PCHC - IT 490 Project <richiepee94@gmail.com>
2018-05-06 23:30:46 -0400

    scp files to deployment

---
+++ IT490 - Midterms/FrontEnd/script1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Checking to make sure file exit 
+
+#file="/home/palabi/frontend.v1.tgz"
+
+file1=$1
+
+if [ -s "$file1" ]
+then
+  echo "Tar file exit, now copying files to Deployment"
+
+  
+  #scp /home/palabi/frontend.v1.tgz carlos@172.20.10.10:/home/carlos/deployment/frontend
+
+else 
+  echo "$0: File '{$file1}' not found"
+tar zcvf frontend.$file1.tgz /var/www/html/frontend
+scp /home/palabi/frontend.$file1.tgz carlos@172.20.10.10:/home/carlos/deployment/frontend
+
+fi
+
+
